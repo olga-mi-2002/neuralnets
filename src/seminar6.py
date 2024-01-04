81c2c994fbaee0622b878212da468638ce41e1b7
olga-mi-2002 <73555913+olga-mi-2002@users.noreply.github.com>
2024-01-04 23:10:05 +0300

    Update seminar6.py

--- src/seminar6.py
+++ src/seminar6.py
@@ -4,8 +4,9 @@ import os
 import zipfile
 import shutil
 from urllib.request import urlretrieve
-
-import tensorflow as tf
+import keras
+from keras import layers
+#import tensorflow as tf
 import boto3
 import dotenv
 
@@ -21,7 +22,7 @@ YOUR_GIT_USER = 'olga-mi-2002'
 def download_data():
     """Pipeline: download and extract data"""
     if not os.path.exists(PATH_TO_DATA_ZIP):
-        print('Downloading data...')
+        print('Downloading data....')
         urlretrieve(DATA_URL, PATH_TO_DATA_ZIP)
     else:
         print('Data is already downloaded!')
@@ -35,15 +36,54 @@ def download_data():
 
 
 def make_model(input_shape, num_classes):
-    model = None
-    return model
+    inputs = keras.Input(shape=input_shape)
+
+    # Entry block
+    x = layers.Rescaling(1.0 / 255)(inputs)
+    x = layers.Conv2D(128, 3, strides=2, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    previous_block_activation = x  # Set aside residual
+
+    for size in [256, 512, 728]:
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(size, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(size, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
+
+        # Project residual
+        residual = layers.Conv2D(size, 1, strides=2, padding="same")(
+            previous_block_activation
+        )
+        x = layers.add([x, residual])  # Add back residual
+        previous_block_activation = x  # Set aside next residual
+
+    x = layers.SeparableConv2D(1024, 3, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    x = layers.GlobalAveragePooling2D()(x)
+    if num_classes == 2:
+        units = 1
+    else:
+        units = num_classes
+
+    x = layers.Dropout(0.25)(x)
+    # We specify activation=None so as to return logits
+    outputs = layers.Dense(units, activation=None)(x)
+    return keras.Model(inputs, outputs)
 
 
 def train():
     """Pipeline: Build, train and save model to models/model_6"""
     # Todo: Copy some code from seminar5 and https://keras.io/examples/vision/image_classification_from_scratch/
     print('Training model')
-
     image_size = (180, 180)
     batch_size = 128
 
@@ -70,41 +110,28 @@ def train():
     )
     model.save(PATH_TO_MODEL)
 
+    image_size = (180, 180)
+    batch_size = 128
 
-def upload():
-    """Pipeline: Upload model to S3 storage"""
-    print('Upload model')
-    zip_model_path = PATH_TO_MODEL+'.zip'
-    shutil.make_archive(base_name=PATH_TO_MODEL,
-                        format='zip',
-                        root_dir=PATH_TO_MODEL)
-
-    config = dotenv.dotenv_values('.env')
-
-    ACCESS_KEY = config['ACCESS_KEY']
-    SECRET_KEY = config['SECRET_KEY']
-
-    client = boto3.client(
-        's3',
-        endpoint_url='https://storage.yandexcloud.net',
-        aws_access_key_id=ACCESS_KEY,
-        aws_secret_access_key=SECRET_KEY
+    train_ds, val_ds = keras.utils.image_dataset_from_directory(
+        "./data/raw/cats_dogs_train/PetImages",
+        validation_split=0.2,
+        subset="both",
+        seed=1337,
+        image_size=image_size,
+        batch_size=batch_size,
     )
 
-    client.upload_file(zip_model_path, BUCKET_NAME, f'{YOUR_GIT_USER}/model_6.zip')
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        prog='src/seminar6.py',
-        description='Typical DL lifecycle pipelines.')
-    parser.add_argument('--download', action='store_true', help='Download images and extract to data/raw directory')
-    parser.add_argument('--train', action='store_true', help='Build, train and save model to models/seminar6_model')
-    parser.add_argument('--upload', action='store_true', help='Upload model to S3 storage')
-    args = parser.parse_args()
-    if args.download:
-        download_data()
-    if args.train:
-        train()
-    if args.upload:
-        upload()
+    model = make_model(input_shape=image_size + (3,), num_classes=2)
+    epochs = 1
+    model.compile(
+        optimizer=keras.optimizers.Adam(3e-4),
+        loss=keras.losses.BinaryCrossentropy(from_logits=True),
+        metrics=[keras.metrics.BinaryAccuracy(name="acc")],
+    )
+    model.fit(
+        train_ds,
+        epochs=epochs,
+        validation_data=val_ds,
+    )
+    model.save(PATH_TO_MODEL)
